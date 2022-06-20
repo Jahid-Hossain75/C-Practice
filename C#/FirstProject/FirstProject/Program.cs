5ce9d293d5f661bef12628a17b8f02a8d740f2d9
Jahid Hossain <107812081+Jahid-Hossain75@users.noreply.github.com>
2022-06-21 01:05:56 +0600

    Add files via upload

---
+++ C#/FirstProject/FirstProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstProject
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            /*int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine("Hello World");
+                i = i + 1;
+            }*/
+
+            /*int i;
+            for (i=1; i<=5; i++)
+            {
+                Console.WriteLine("Hello World");
+            }*/
+
+
+            /*int i = 1;
+            do
+            {
+                //product = n * i;
+                Console.WriteLine("Hello World");
+                i++;
+            } while (i <= 5);*/
+
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+}
